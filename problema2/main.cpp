168356ec6506775e5c1043f0b1340249d9f6f8ee
IanDuque <ian.duquep@udea.edu.co>
2025-10-14 13:34:03 -0500

    implementacion de funciones para codificar y decodificar

--- problema2/main.cpp
+++ problema2/main.cpp
@@ -92,6 +92,208 @@ void concatenar(char *cadena_1, char *cadena_2, int tam_1, int tam_2)
         cadena_1[i + tam_1] = cadena_2[i];
 }
 
+//codificacion y decodificacion
+char * decodificar(char * cad_codificada,int tam_cad,int bits_por_bloque)
+{
+    char * original = new char [tam_cad], *texto = new char [tam_cad/8] ;
+
+    int cont_1 = 0,cont_0 = 0,cont_bloques = 0;
+
+    //se decodifica la primera linea
+
+
+    for(int i = 0; i < bits_por_bloque ; i++)
+    {
+        if(cad_codificada[i] == '1')
+            original[i] = '0';
+        else
+            original[i] = '1';
+    }
+
+    for(int i = 0; i < bits_por_bloque ; i++)
+    {
+        if(original[i] == '1')
+            cont_1++;
+        else
+            cont_0++;
+    }
+
+    cont_bloques++;
+
+    //se decodifican las lineas siguientes
+    while(cont_bloques < tam_cad/bits_por_bloque)
+    {
+        if(cont_0 == cont_1)
+        {
+            for(int i = bits_por_bloque * cont_bloques; i < (bits_por_bloque * cont_bloques)+ bits_por_bloque ; i++)
+            {
+                if(cad_codificada[i] == '1')
+                    original[i] = '0';
+                else
+                    original[i] = '1';
+            }
+        }
+
+        else if(cont_0 > cont_1)
+        {
+            for(int i = (bits_por_bloque * cont_bloques); i < (bits_por_bloque * cont_bloques)+ bits_por_bloque ; i++)
+            {
+                if(((i+1 - (bits_por_bloque * cont_bloques))%2) == 0 )
+                {
+                    if(cad_codificada[i] == '1')
+                        original[i] = '0';
+                    else
+                        original[i] = '1';
+                }
+                else
+                    original[i] = cad_codificada[i];
+            }
+        }
+
+        else
+        {
+            for(int i = (bits_por_bloque * cont_bloques); i < (bits_por_bloque * cont_bloques)+ bits_por_bloque ; i++)
+            {
+                if(((i+1 - (bits_por_bloque * cont_bloques))%3) == 0 )
+                {
+                    if(cad_codificada[i] == '1')
+                        original[i] = '0';
+                    else
+                        original[i] = '1';
+                }
+                else
+                    original[i] = cad_codificada[i];
+            }
+        }
+
+        cont_0 = 0;
+        cont_1 = 0;
+
+
+        for(int i = (bits_por_bloque * cont_bloques); i < (bits_por_bloque * cont_bloques)+ bits_por_bloque ; i++)
+        {
+            if(original[i] == '1')
+                cont_1++;
+            else
+                cont_0++;
+        }
+
+        cont_bloques++;
+
+    }
+
+    cont_1 = 0;
+
+    for(int i = 0; i < tam_cad ; i+= 8)
+    {
+        texto [cont_1] = binario_a_letra(&original[i]);
+        cont_1++;
+    }
+
+    delete[] original;
+
+    return texto;
+}
+
+char *codificar(char * cadena, int tam_cadena, int n)
+{
+    int cont_0 = 0,cont_1 = 0,cont_bloques = 0;
+    char * nueva = new char [tam_cadena * 8],* codificado = new char [tam_cadena * 8], binario[8];
+
+
+    // se hace transforma a binario el primer caracter
+    letra_a_binario(cadena[0],binario);
+
+    concatenar(nueva,binario,0,8);
+
+    // se transforma a binario el resto de caracteres
+    for (int i = 0; i < tam_cadena; i++)
+    {
+        letra_a_binario(cadena[i+1],binario);
+        concatenar(nueva,binario,(i*8)+8,8);
+    }
+
+    for (int i = 0; i < tam_cadena*8; i++)
+        codificado[i] = nueva[i];
+
+    while(cont_bloques < (tam_cadena*8)/n)
+    {
+
+
+        if(cont_bloques == 0)
+        {
+            //se codifica el primer bloque
+
+            for(int i = 0; i < n; i++) // se cuentan los 1 y 0
+            {
+                if(nueva[i] == '1')
+                    cont_1++;
+                else
+                    cont_0++;
+            }
+
+            for (int i = 0; i < n; i++) // se intercambian los 1 y 0
+            {
+                if(nueva[i] == '1')
+                    codificado[i] = '0';
+                else
+                    codificado[i] = '1';
+            }
+        }
+
+        else
+        {
+
+            for(int i = n*cont_bloques - n; i < n*cont_bloques; i++) // se cuentan los 1 y 0
+            {
+                if(nueva[i] == '1')
+                    cont_1++;
+                else
+                    cont_0++;
+            }
+
+            if(cont_0 == cont_1)
+            {
+                for (int i = n*cont_bloques; i < (n*cont_bloques)+n; i++) // se intercambian los 1 y 0
+                {
+                    if(nueva[i] == '1')
+                        codificado[i] = '0';
+                    else
+                        codificado[i] = '1';
+                }
+            }
+
+            else if(cont_0 > cont_1)
+            {
+                for (int i = (n*cont_bloques) + 1; i < (n*cont_bloques) + n; i+=2)
+                {
+                    if(nueva[i] == '1')
+                        codificado[i] = '0';
+                    else
+                        codificado[i] = '1';
+                }
+            }
+
+            else
+            {
+                for (int i = (n*cont_bloques) + 2; i < (n*cont_bloques) +n; i+=3)
+                {
+                    if(nueva[i] == '1')
+                        codificado[i] = '0';
+                    else
+                        codificado[i] = '1';
+                }
+            }
+        }
+        cont_bloques ++;
+        cont_0 = 0;
+        cont_1 = 0;
+
+    }
+
+    return codificado;
+}
+
 int main()
 {
     int opcion = 0;
